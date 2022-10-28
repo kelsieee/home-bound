804763cf1874cbb3e495ec12aab9e607500b02b9
kelsieee <jenniferkelsie73@gmail.com>
2022-10-28 08:51:31 +0800

    dropdown fix

--- navComponent.js
+++ navComponent.js
@@ -11,11 +11,13 @@ main.component('nav-component', {
     
     
     template: `
+
+
     <body>
     <nav class="navbar navbar-expand-lg bg-light ">
         <!-- <div class="row"> -->
             
-        <div class="container-fluid " >
+        <div class="container-fluid" >
             <ul class=" list-unstyled navbar   my-1 mx-3 my-lg-0 ">
 
                 <li>
@@ -91,12 +93,12 @@ main.component('nav-component', {
            
             <li class="nav-item dropdown">
                 <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                    <svg xmlns="http://www.w3.org/2000/svg" width="35" height="35" fill="black" class="bi bi-sliders2" viewBox="0 0 16 16">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="55" height="35" fill="black" class="bi bi-sliders2" viewBox="0 0 16 16">
                         <path fill-rule="evenodd" d="M10.5 1a.5.5 0 0 1 .5.5v4a.5.5 0 0 1-1 0V4H1.5a.5.5 0 0 1 0-1H10V1.5a.5.5 0 0 1 .5-.5ZM12 3.5a.5.5 0 0 1 .5-.5h2a.5.5 0 0 1 0 1h-2a.5.5 0 0 1-.5-.5Zm-6.5 2A.5.5 0 0 1 6 6v1.5h8.5a.5.5 0 0 1 0 1H6V10a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5ZM1 8a.5.5 0 0 1 .5-.5h2a.5.5 0 0 1 0 1h-2A.5.5 0 0 1 1 8Zm9.5 2a.5.5 0 0 1 .5.5v4a.5.5 0 0 1-1 0V13H1.5a.5.5 0 0 1 0-1H10v-1.5a.5.5 0 0 1 .5-.5Zm1.5 2.5a.5.5 0 0 1 .5-.5h2a.5.5 0 0 1 0 1h-2a.5.5 0 0 1-.5-.5Z"/>
                     </svg>
                 </a>
 
-                <ul class="dropdown-menu dropdown-menu-lg-end ">
+                <ul class="dropdown-menu dropdown-menu-lg-end">
                 <li><h4>Roomies</h4></li>
                 <li><a class="dropdown-item" href="#"><h6>Cleanliness</h6></a></li>
                   <div class="form-check form-check-inline">
