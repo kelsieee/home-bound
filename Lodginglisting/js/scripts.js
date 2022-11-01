ba2188c4ffcf44c9c28d994257d85acca83c2f88
kelsieee <jenniferkelsie73@gmail.com>
2022-11-01 23:59:36 +0800

    YAY can upload image nicely now

--- Lodginglisting/js/scripts.js
+++ Lodginglisting/js/scripts.js
@@ -1,34 +1,18 @@
-/*!
-* Start Bootstrap - Scrolling Nav v5.0.5 (https://startbootstrap.com/template/scrolling-nav)
-* Copyright 2013-2022 Start Bootstrap
-* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-scrolling-nav/blob/master/LICENSE)
-*/
-//
-// Scripts
-// 
+// function readURL(input) {
+//     if (input.files && input.files[0]) {
+//         var reader = new FileReader();
 
-window.addEventListener('DOMContentLoaded', event => {
+//         reader.onload = function (e) {
+//             $('#blah').attr('src', e.target.result);
+//         }
 
-    // Activate Bootstrap scrollspy on the main nav element
-    const mainNav = document.body.querySelector('#mainNav');
-    if (mainNav) {
-        new bootstrap.ScrollSpy(document.body, {
-            target: '#mainNav',
-            offset: 74,
-        });
-    };
+//         reader.readAsDataURL(input.files[0]);
+//     }
+// }
 
-    // Collapse responsive navbar when toggler is visible
-    const navbarToggler = document.body.querySelector('.navbar-toggler');
-    const responsiveNavItems = [].slice.call(
-        document.querySelectorAll('#navbarResponsive .nav-link')
-    );
-    responsiveNavItems.map(function (responsiveNavItem) {
-        responsiveNavItem.addEventListener('click', () => {
-            if (window.getComputedStyle(navbarToggler).display !== 'none') {
-                navbarToggler.click();
-            }
-        });
-    });
-
-});
+// $("#imgInp").change(function(){
+//     readURL(this);
+// });
+function img_pathUrl(input){
+    $('#img_url')[0].src = (window.URL ? URL : webkitURL).createObjectURL(input.files[0]);
+}
