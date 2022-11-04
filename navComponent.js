227635e021aa18fde9fa439e4a8564c59356a8f3
kelsieee <jenniferkelsie73@gmail.com>
2022-11-05 04:59:33 +0800

    finish rerouting except for login , register

--- navComponent.js
+++ navComponent.js
@@ -28,16 +28,16 @@ main.component('nav-component', {
             <ul class=" list-unstyled navbar my-1 mx-3 my-lg-0 ">
 
             <li class="nav-item text-center mx-5 ">
-                <a class="nav-link" href="/register,login/register.html">
+                <a class="nav-link" href="/register,login/login.html">
                     <svg xmlns="http://www.w3.org/2000/svg" width="44" height="44" fill="black" class="bi bi-person-fill" viewBox="0 0 16 16">
                         <path d="M3 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1H3zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6z"/>
                     </svg>
                 
                 </a>
-                Profile
+                Login
             </li>
                 <li class="nav-item text-center">
-                    <a class="nav-link" href="/home.html">
+                    <a class="nav-link" href="/home.html#project-area">
                     <svg xmlns="http://www.w3.org/2000/svg" width="35" height="44" fill="currentColor" class="bi bi-house-door-fill" viewBox="0 0 16 16">
                     <path d="M6.5 14.5v-3.505c0-.245.25-.495.5-.495h2c.25 0 .5.25.5.5v3.5a.5.5 0 0 0 .5.5h4a.5.5 0 0 0 .5-.5v-7a.5.5 0 0 0-.146-.354L13 5.793V2.5a.5.5 0 0 0-.5-.5h-1a.5.5 0 0 0-.5.5v1.293L8.354 1.146a.5.5 0 0 0-.708 0l-6 6A.5.5 0 0 0 1.5 7.5v7a.5.5 0 0 0 .5.5h4a.5.5 0 0 0 .5-.5z"/>
                         </svg>
