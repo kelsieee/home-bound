c1f37cb872972fe3d7352dc45631e038468105ac
kelsieee <jenniferkelsie73@gmail.com>
2022-11-08 00:05:54 +0800

    just testing

---
+++ testingnewfilter/testingfilter2/index.js
@@ -0,0 +1,46 @@
+
+filterSelection("all")
+
+function filterSelection(c) {
+    var x, i;
+    x = document.getElementsByClassName("filterDiv");
+    if (c == "all") c = "";
+    
+    
+    for (i = 0; i < x.length; i++) {
+        w3RemoveClass(x[i], "show");
+        console.log(x[i].className.indexOf(c))
+        if (x[i].className.indexOf(c) > -1) w3AddClass(x[i], "show");
+    }
+}
+
+// Show filtered elements
+function w3AddClass(element, name) {
+    var i, arr1, arr2;
+    arr1 = element.className.split(" ");
+    arr2 = name.split(" ");
+
+    for (i = 0; i < arr2.length; i++) {
+        if (arr1.indexOf(arr2[i]) == -1) {
+            element.className += " " + arr2[i];
+
+        }
+    }
+}
+
+// Hide elements that are not selected
+function w3RemoveClass(element, name) {
+    var i, arr1, arr2;
+    arr1 = element.className.split(" ");
+    arr2 = name.split(" ");
+
+    
+    for (i = 0; i < arr2.length; i++) {
+        while (arr1.indexOf(arr2[i]) > -1) {
+            arr1.splice(arr1.indexOf(arr2[i]), 1);
+            // console.log(arr1)
+        }
+    }
+    element.className = arr1.join(" ");
+}
+
