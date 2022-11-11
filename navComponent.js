d96707f4d8c606b138045342c4921d99b4d54335
LAETITIA <JEHONG.2021@SCIS.SMU.EDU.SG>
2022-11-11 22:51:38 +0800

    home brand works

--- navComponent.js
+++ navComponent.js
@@ -7,7 +7,7 @@ main.component("nav-component", {
 
 
         <nav class="navbar navbar-expand-lg navbar-light " style='background-color:#dbe6da'>
-        <a class="navbar-brand" href="#">
+        <a class="navbar-brand" href="../home.html">
         <img  class='mx-4 mt-3'src='/images/Screenshot 2022-11-07 at 12.40.27 AM.png' style='width : 220px; margin-left:10px'>
         </a>
         <button class="navbar-toggler mx-4" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
